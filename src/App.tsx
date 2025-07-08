79e3d262e542e76843d0e9fde7ec65d72dd28f0b
Gabriel <gappaneo@gmail.com>
2025-07-08 17:09:58 +0800

    adding basic chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { parseCSV } from "./utilities/parseCSV";
+import { ColorType, createChart, AreaSeries } from "lightweight-charts";
 
 function App() {
   useEffect(() => {
@@ -12,9 +13,42 @@ function App() {
       .catch((err) => console.error("Error loading CSV:", err));
   }, []);
 
+  const chartContainerRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (chartContainerRef.current) {
+      const chart = createChart(chartContainerRef.current, {
+        layout: {
+          background: { type: ColorType.Solid, color: "white" },
+        },
+        width: 500,
+        height: 200,
+      });
+
+      const newSeries = chart.addSeries(AreaSeries, {
+        lineColor: "#2962FF",
+        topColor: "#2962FF",
+        bottomColor: "rgba(41, 98, 255, 0.28)",
+      });
+      newSeries.setData([
+        { time: "2018-12-22", value: 32.51 },
+        { time: "2018-12-23", value: 31.11 },
+        { time: "2018-12-24", value: 27.02 },
+        { time: "2018-12-25", value: 27.32 },
+        { time: "2018-12-26", value: 25.17 },
+        { time: "2018-12-27", value: 28.89 },
+        { time: "2018-12-28", value: 25.46 },
+        { time: "2018-12-29", value: 23.92 },
+        { time: "2018-12-30", value: 22.68 },
+        { time: "2018-12-31", value: 22.67 },
+      ]);
+      return () => chart.remove();
+    }
+  }, []);
+
   return (
     <div>
       <h1>Trading Dashboard</h1>
+      <div ref={chartContainerRef}></div>
     </div>
   );
 }
